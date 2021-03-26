bc3aa15a00e9cf764e9152ba03e1be4185f3f771
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-03-26 09:27:07 +0100

    Update oblig2.py
    Added method structure

--- oblig2.py
+++ oblig2.py
@@ -6,4 +6,30 @@ import requests
 
 app = Flask(__name__)
 api = Api(app)
-BASE = "/"
+
+
+
+class User(Resource):
+    BASE = "/api/users/"
+
+    @app.route(BASE)
+    def get(self):
+        pass
+
+    @app.route(BASE + "<userID>")
+    def get(self, userID):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self, userID):
+        pass
+
+
+class Chat_room(Resource):
+    pass
+
+
+api.add_resource(User, "/api/users")
+api.add_resource(Chat_room, "/api/room")
